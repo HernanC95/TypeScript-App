6465216184e7be6691210d991c051e51344ef54c
HernanC95 <cavalieri.h95@gmail.com>
2023-04-21 10:35:35 -0300

    tipos Basicos

--- reforzamiento/src/typescript/TiposBasicos.tsx
+++ reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,7 +1,16 @@
 export default function TiposBasicos() {
+    const nombre: string = 'Fernando';
+    const edad: number = 35;
+    const estaActivo: boolean = true;
+    
+    const poderes: string[] = []; //'velocidad','Volar','Respirar en el agua'
+
   return (
     <>
-    <h3>Tipos basicos</h3>
+        <h3>Tipos basicos</h3>
+        {nombre}, {edad}, { (estaActivo) ? 'activo' : 'no activo' }
+        <br />
+        {poderes.join(', ')}
     </>
   )
 }
